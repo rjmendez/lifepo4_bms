88c12272cf23453b32002cda44d9e311f0edf964
rjmendez <robertjmendez@gmail.com>
2018-01-19 12:42:13 -0600

    Headless operation.

--- plot_battery.py
+++ plot_battery.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys, datetime
 all_raw = open(sys.argv[1], 'r')
@@ -12,15 +15,31 @@ totalv = []
 
 for line in all_raw:
     if 'voltage cell 0: ' in line:
-        cell0v.append(float(line.replace('voltage cell 0: ', '')[:-4]))
+        try:
+            cell0v.append(float(line.replace('voltage cell 0: ', '')[:-4]))
+        except:
+            print('Malformed data: ' + line)
     if 'voltage cell 1: ' in line:
-        cell1v.append(float(line.replace('voltage cell 1: ', '')[:-4]))
+        try:
+            cell1v.append(float(line.replace('voltage cell 1: ', '')[:-4]))
+        except:
+            print('Malformed data: ' + line)
     if 'voltage cell 2: ' in line:
-        cell2v.append(float(line.replace('voltage cell 2: ', '')[:-4]))
+        try:
+            cell2v.append(float(line.replace('voltage cell 2: ', '')[:-4]))
+        except:
+            print('Malformed data: ' + line)
     if 'voltage cell 3: ' in line:
-        cell3v.append(float(line.replace('voltage cell 3: ', '')[:-4]))
+        try:
+            cell3v.append(float(line.replace('voltage cell 3: ', '')[:-4]))
+        except:
+            print('Malformed data: ' + line)
     if 'voltage total: ' in line:
-        totalv.append(float(line.replace('voltage total: ', '')[:-4]))
+        try:
+            totalv.append(float(line.replace('voltage total: ', '')[:-4]))
+        except:
+            print('Malformed data: ' + line)
+
 plt.figure(figsize=(15, 15))
 plt.tight_layout()
 plt.plot(totalv)
@@ -34,4 +53,4 @@ plt.plot(cell2v, color='green')
 plt.plot(cell3v, color='cyan')
 plt.xlabel('C0 = blue C1 = red C2 = green C3 = cyan')
 plt.savefig(time+'_cell_voltage.png')
-#plt.show()
+#plt.show()l
